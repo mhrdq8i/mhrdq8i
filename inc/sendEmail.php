454ac3282eb8f2aa75c063dc5f7caf95af90229a
mehrdad <mehrdad.pc@gmail.com>
2020-07-16 20:43:16 +0430

    Edit my profile description Add job history description Add some skill

--- inc/sendEmail.php
+++ inc/sendEmail.php
@@ -38,7 +38,7 @@ if($_POST) {
    $from =  $name . " <" . $email . ">";
 
    // Email Headers
-	$headers = "From: " . $from . "\r\n";
+	$headers  = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $email . "\r\n";
  	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
@@ -56,7 +56,7 @@ if($_POST) {
 
 	else {
 
-		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response  = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
 		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
 		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
 		
